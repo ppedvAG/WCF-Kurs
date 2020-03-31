d5e7f8e003bb1cdb9dbdef27bb7f88db11b6e128
André <ruland@outlook.de>
2020-03-31 11:04:49 +0200

    Ui fertig und hübch

---
+++ WcfPowerChat/WcfPowerChat.Client/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using AdonisUI;
+using System.Windows;
+
+namespace WcfPowerChat.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
